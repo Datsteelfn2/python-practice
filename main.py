c5b728391facc18a6c348adb90301442b44adb5f
Elhadj D <Datsteelfn@gamil.com>
2024-12-26 17:52:31 -0500

    just put a font on the screen

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ pygame.init()
 width,height=800,400
 screen=pygame.display.set_mode((width,height))
 #how to display images below
+
+test_font=pygame.font.Font("font/Pixeltype.ttf",50)
+
 sky_surface=pygame.image.load('graphics/sky.png')
 ground_surface=pygame.image.load('graphics/ground.png')
 
+text_surface=test_font.render("Python practice",False,'Black')
 
 clock=pygame.time.Clock()
 run=True
@@ -21,6 +25,7 @@ while run:
             run=False
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
     #blit allows us to place one surface on another surface, here we put our surface on top of our screen(variable) surface
     clock.tick(60)
     pygame.display.update()
