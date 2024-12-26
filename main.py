a93586ebb79cfc843685661954c7cbc0770d3cc0
Elhadj D <Datsteelfn@gamil.com>
2024-12-26 16:57:31 -0500

    added all the images

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ while run:
     for even in pygame.event.get():
         if even.type==pygame.QUIT:
             run=False
-
     screen.blit(surface,(200,100))
     #blit allows us to place one surface on another surface, here we put our surface on top of our screen(variable) surface
     clock.tick(60)
