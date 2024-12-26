da461bbe23432573e3221afadaec11d16cd5f4a4
Elhadj D <Datsteelfn@gamil.com>
2024-12-26 16:47:59 -0500

    set up the enviroment

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+# neccessary to use pygame
+
+
+
+
+width,height=800,400
+screen=pygame.display.set_mode((width,height))
+surface=pygame.image.load()
+
+
+
+clock=pygame.time.Clock()
+run=True
+while run:
+   
+    for even in pygame.event.get():
+        if even.type==pygame.QUIT:
+            run=False
+
+    screen.blit(surface,(200,100))
+    #blit allows us to place one surface on another surface, here we put our surface on top of our screen(variable) surface
+    clock.tick(60)
+    pygame.display.update()
+    # this updates everything including our screen variable
