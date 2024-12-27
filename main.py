fe4fb70aed50abf79e14ed4c968ac9d4dfc8b2a0
Elhadj D <Datsteelfn@gamil.com>
2024-12-26 19:39:17 -0500

    moved the snail to the right, a simple animation

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ test_font=pygame.font.Font("font/Pixeltype.ttf",50)
 
 sky_surface=pygame.image.load('graphics/sky.png')
 ground_surface=pygame.image.load('graphics/ground.png')
-
 text_surface=test_font.render("Python practice",False,'Black')
-
+snail_surface=pygame.image.load("graphics/snail/snail1.png")
 clock=pygame.time.Clock()
+
+
+snail_x_pos=600
+vel=5
 run=True
 while run:
    
@@ -26,6 +29,9 @@ while run:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
+    snail_x_pos-=vel
+    screen.blit(snail_surface,(snail_x_pos,270))
+
     #blit allows us to place one surface on another surface, here we put our surface on top of our screen(variable) surface
     clock.tick(60)
     pygame.display.update()
